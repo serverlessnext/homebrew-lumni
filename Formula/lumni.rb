17b71d679ca4f73025879d26b287cf51f010503d
GitHub Action <github-action@serverlessnext.com>
2024-06-19 20:40:46 +0000

    Update formula to release

--- Formula/lumni.rb
+++ Formula/lumni.rb
@@ -4,17 +4,17 @@ class Lumni < Formula
 
   # Define the base URL and commit hash as variables
   base_url = "https://github.com/serverlessnext/lumni/releases/download"
-  release_tag = "b0367ea"
+  release_tag = "9c14f04"
 
   if OS.mac? && Hardware::CPU.intel?
     url "#{base_url}/#{release_tag}/lumni-x86_64-apple-darwin-#{release_tag}.tar.gz"
-    sha256 "{{ SHA256_MAC_INTEL }}"
+    sha256 "37a19d7c6a466814c5f5dc2d573a76055bf567833479f57aae735e3e62a497cc"
   elsif OS.mac? && Hardware::CPU.arm?
     url "#{base_url}/#{release_tag}/lumni-aarch64-apple-darwin-#{release_tag}.tar.gz"
-    sha256 "{{ SHA256_MAC_ARM }}"
+    sha256 "347396b7ed74f217273cee76834fa3bcb20e4f8f177b765f7e0812b900ca46ec"
   elsif OS.linux && Hardware::CPU.intel?
     url "#{base_url}/#{release_tag}/lumni-x86_64-unknown-linux-gnu-#{release_tag}.tar.gz"
-    sha256 "2c2c36759c8014cc190eb289c97da5e04e8f5ff67d29f73d1ab532d75960796f"
+    sha256 "1be09b9216c7d7e7993c126839384a45a131176711ec68f13f5eb4b10b579b0b"
   end
 
   def install
