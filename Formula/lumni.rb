00ce767b360489e729a982609c454d20fdaef373
GitHub Action <github-action@serverlessnext.com>
2024-06-30 16:11:51 +0000

    Update formula to release 2kzfhhtd

--- Formula/lumni.rb
+++ Formula/lumni.rb
@@ -1,19 +1,19 @@
 class Lumni < Formula
   desc "Lumni CLI"
   homepage "https://github.com/serverlessnext/lumni"
-  release_tag = "2kzfhaav"
+  release_tag = "2kzfhhtd"
   version release_tag
   base_url = "https://github.com/serverlessnext/lumni/releases/download/#{release_tag}"
 
   if OS.mac? && Hardware::CPU.intel?
     url "#{base_url}/lumni-x86_64-apple-darwin-#{release_tag}.tar.gz"
-    sha256 "83408ad738be56fae536c68e38cc88ead058dea04357adc61ac686748273cff1"
+    sha256 "576adbcd731b85656af0c9b057252520adcfa9b5e543d587387ed66b1a9fd77f"
   elsif OS.mac? && Hardware::CPU.arm?
     url "#{base_url}/lumni-aarch64-apple-darwin-#{release_tag}.tar.gz"
-    sha256 "184644b3596c9311f45b47b962428f014f396d98bd6d987da163fb1f25ab9127"
+    sha256 "b72df31d1c8622750fd8bd62044dff2480e4f204e9c1d304f08dc7d99626defb"
   elsif OS.linux && Hardware::CPU.intel?
     url "#{base_url}/lumni-x86_64-unknown-linux-gnu-#{release_tag}.tar.gz"
-    sha256 "c528e778ec1fd986ec0be73d7a207694abc72336abab968fa8e85bb0df0c31b9"
+    sha256 "319b59f5adb4e12f580b31a31462d4b0ad14953cd8794c935836984452121e2d"
   end
 
   def install
